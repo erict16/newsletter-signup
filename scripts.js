55a72ea1ec97bb427e8fd18e45a1b3b8997e92b3
Eric Tan <ytan5758@uni.sydney.edu.au>
2023-12-28 12:03:32 +1100

    reminder when email are wrong

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function validateForm(event) {
+      // Get the email input value
+      var emailInput = document.getElementById('emailInput');
+      var email = emailInput.value;
+
+      // Define a regular expression for email validation
+      var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+      // Check if the email matches the regular expression
+      if (!emailRegex.test(email)) {
+        // If not valid, prevent form submission and show an error message
+        alert('Please enter a valid email address.');
+        event.preventDefault();
+      } else {
+        alert("hello world");
+      }
+}
