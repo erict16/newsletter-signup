dd6c73a414bc7d34118ecde67b5db0f3daf9076a
Eric Tan <ytan5758@uni.sydney.edu.au>
2023-12-29 16:04:53 +1100

    project finished!!

--- scripts.js
+++ scripts.js
@@ -1,17 +1,37 @@
 function validateEmail(event) {
-      // Get the email input value
-      var emailInput = document.getElementById('emailInput');
-      var email = emailInput.value;
+  // Get the email input value
+  var emailInput = document.getElementById('emailInput');
+  var email = emailInput.value;
+  if (!emailInput.checkValidity()) {
+        // Display an error message or perform other actions
+        alert("Invalid email address");
+        event.preventDefault(); // Prevent form submission
+  } else {
+    successMessage(email);
+  }
+  
 
-      // Define a regular expression for email validation
-      var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-      // Check if the email matches the regular expression
-      if (!emailRegex.test(email)) {
-        // If not valid, prevent form submission and show an error message
-        alert('Please enter a valid email address.');
-        event.preventDefault();
-      } else {
-        alert("hello world");
-      }
 }
+
+function successMessage(email) {
+  var container = document.querySelector(".container");
+  var successMessage = document.querySelector(".success-container");
+  container.style.display = 'none';
+  successMessage.style.display = 'flex';
+//   Adding message to the HTML
+
+  var message = document.getElementById("success-message");
+  message.innerHTML = "A confirmation email has been sent to\n" +
+    "<b>" + email + "</b>. " + "Please open it and click the button inside to\n" +
+    "        confirm your subscription. Dismiss message"
+
+}
+
+var buttonBack = document.getElementById("success-button");
+buttonBack.addEventListener("click", function () {
+  window.location.reload();
+})
+
+
+
+
